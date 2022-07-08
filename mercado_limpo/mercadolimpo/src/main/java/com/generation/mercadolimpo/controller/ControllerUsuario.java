f8b14122d39aa320984ce6116f420c62dfc92b19
João Victor Lopes <91668424+ProjectzJV@users.noreply.github.com>
2022-07-08 19:22:00 -0300

    Projeto integrador

---
+++ mercado_limpo/mercadolimpo/src/main/java/com/generation/mercadolimpo/controller/ControllerUsuario.java
@@ -0,0 +1,71 @@
+package com.generation.mercadolimpo.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.mercadolimpo.model.Usuario;
+import com.generation.mercadolimpo.repository.RepositoryUsuario;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ControllerUsuario {
+
+	@Autowired
+	private RepositoryUsuario repository;
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> inserirUsuario(@RequestBody @Valid Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> atualizarUsuario(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok().body(repository.save(usuario));
+	}
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Usuario> buscaPorId(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+
+	/*
+	 * 
+	 * 
+	 * @GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> buscaPornome(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@GetMapping("/email/{email}")
+	public ResponseEntity<List<Usuario>> getByEmail(@PathVariable String email) {
+		return ResponseEntity.ok(repository.findAllByEmailContainingIgnoreCase(email));
+	}*/
+	
+
+	@DeleteMapping("/id/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
+}
